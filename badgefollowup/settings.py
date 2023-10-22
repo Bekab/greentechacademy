ef8f6a97118959aa910ed14caa5e8ccb5ae614c5
Yohannes251 <yohannes.z251@gmail.com>
2023-10-22 12:51:29 +0300

    Updated site from forked version

--- badgefollowup/settings.py
+++ badgefollowup/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -119,9 +122,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'badgefollowup/static')
-]
+STATICFILES_DIRS = [BASE_DIR, 'badgefollowup/static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
