cd5a224ef5ebae09f0888d4dd96e058cf9060906
Yohannes251 <yohannes.z251@gmail.com>
2023-10-25 17:09:49 +0300

    Updated UI and added a new model

--- pages/urls.py
+++ pages/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('cohort', views.cohort, name='cohort')
+    path('cohort', views.cohort, name='cohort'),
+    path('students', views.students, name='students'),
 ]
